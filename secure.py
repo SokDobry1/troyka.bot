76baf5bdc1926e8ecb8fa4b7e81000dbddaf25c5
SokDobry1 <officialaccalf@gmail.com>
2022-09-15 16:14:08 +0300

    Изменена структура. Начинаю работу над Mikrotik api

---
+++ secure.py
@@ -0,0 +1,9 @@
+headers = {"Authorization": "Basic "}
+
+class Mikrotik:
+    def __init__(self):
+        self.login = ""
+        self.password = ""
+        self.dst = ""
+
+mikrotik = Mikrotik()
