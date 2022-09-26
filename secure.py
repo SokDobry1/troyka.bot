a84e0d957ed6f6eca7e80d2db6672feeb19f29f8
SokDobry1 <officialaccalf@gmail.com>
2022-09-26 03:31:47 +0300

    Обработка ошибок и исправление под копиляцию в .exe

--- secure.py
+++ secure.py
@@ -1,11 +1,11 @@
-headers = {"Authorization": "Basic U29rRG9icnk6MTIzNTEyMzVhcA"}
+headers = {"Authorization": "Basic ****************"} 
 #Переведите логин и пароль в Basic64 в формате логин:пароль
 
 class Mikrotik:
     def __init__(self):
-        self.login = "Alf" #Логин микротик
-        self.password = "12351235ap" #Пароль микротик
-        self.dst = "10.24.203.253" #IP адрес Mikrotik
+        self.login = "********" #Логин микротик
+        self.password = "********" #Пароль микротик
+        self.dst = "********" #IP адрес Mikrotik
 
 
 
